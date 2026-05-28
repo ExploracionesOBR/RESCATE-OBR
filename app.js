3aab7077e720b6dcd962d32e403992c4af57a34f
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-05-27 21:23:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+window.auth = auth;   // 👈 EXPONER GLOBALMENTE
 const db = getFirestore(app);
 window.db = db;
 const rtdb = getDatabase(app);
