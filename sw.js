64352f67d483fb3f242ef115249602c58eadc40f
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-06-25 21:22:21 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ===== IMPORTAR EL SERVICE WORKER DE ONESIGNAL (ÚNICA LÍNEA NUEVA) =====
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE_NAME = 'obr-cache-v70';
+const CACHE_NAME = 'obr-cache-v71';
 const BASE_PATH = '/RESCATE-OBR';
 
 
