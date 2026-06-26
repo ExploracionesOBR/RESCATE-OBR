ff8b3a3f59b74614285158a91d70a4e86bb7bd53
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-06-25 21:24:14 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-// ===== IMPORTAR EL SERVICE WORKER DE ONESIGNAL (ÚNICA LÍNEA NUEVA) =====
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
-
 const CACHE_NAME = 'obr-cache-v71';
 const BASE_PATH = '/RESCATE-OBR';
 
@@ -20,7 +17,8 @@ const ALL_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
 ];
-
+// ===== IMPORTAR EL SERVICE WORKER DE ONESIGNAL (ÚNICA LÍNEA NUEVA) =====
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
