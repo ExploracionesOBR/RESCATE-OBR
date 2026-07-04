9d100f8be6863058a0dabc84df6f6e092bc38ab1
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-07-04 16:43:46 -0700

    Update cache version from v11 to v17

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // VERSIÓN DE LA CACHÉ
 // ============================================================
-const CACHE_NAME = 'obr-cache-v11';
+const CACHE_NAME = 'obr-cache-v17';
 const BASE_PATH = '/RESCATE-OBR';
 
 const ALL_FILES = [
