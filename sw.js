15449ffe2b015c5178770ed204cf79794a645747
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-07-11 16:58:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // VERSIÓN DE LA CACHÉ
 // ============================================================
-const CACHE_NAME = 'obr-cache-v39';
+const CACHE_NAME = 'obr-cache-v40';
 const BASE_PATH = '/RESCATE-OBR';
 
 const ALL_FILES = [
