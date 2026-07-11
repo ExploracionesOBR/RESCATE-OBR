8f8647472affbb5267855f4691e0da1c08fa9580
ExploracionesOBR <eder.villarreal26@gmail.com>
2026-07-11 02:48:59 -0600

    Update cache version from v37 to v35

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ============================================================
 // VERSIÓN DE LA CACHÉ
 // ============================================================
-const CACHE_NAME = 'obr-cache-v37';
+const CACHE_NAME = 'obr-cache-v35';
 const BASE_PATH = '/RESCATE-OBR';
 
 const ALL_FILES = [
